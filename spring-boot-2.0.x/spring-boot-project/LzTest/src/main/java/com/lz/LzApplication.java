44e65cb023ad39a71ba61108e3bd029184dec2f0
lz <lz@163.com>
2019-09-10 13:34:36 +0800

    添加测试模块

---
+++ spring-boot-2.0.x/spring-boot-project/LzTest/src/main/java/com/lz/LzApplication.java
@@ -0,0 +1,14 @@
+package com.lz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(LzApplication.class);
+//		application.addListeners(new ApplicationStartup());
+		application.run(args);
+	}
+}
